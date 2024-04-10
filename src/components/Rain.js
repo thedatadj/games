9217d9c1bb2c7c359aba9d9470431753c61f3f4e
unknown <edavid.rd@gmail.com>
2024-04-10 11:29:56 -0400

    Create character and enviroment

--- src/components/Rain.js
+++ src/components/Rain.js
@@ -2,9 +2,9 @@ import Bg from './rain/Bg'
 const Rain = () =>
 {
     return (
-        <>
+        <div className='rainy'>
             <Bg />
-        </>
+        </ div>
     )
 }
 export default Rain
