68f64bfaa90fff25d92929eb8a1843ae35bddda6
unknown <edavid.rd@gmail.com>
2024-04-12 11:17:56 -0400

    Finish initial logic

--- src/components/devilsAndSaints/Devils.js
+++ src/components/devilsAndSaints/Devils.js
@@ -1,17 +1,22 @@
-import './style.css'
+import './devils.css'
 // functions
-import { useState } from 'react' 
+import { useState } from 'react'
+// Variables
+var sOnBoat = false;
+var dOnBoat = false;
+var boatLeft = true;
+// Objects
+const boat = {x: -190, y: 260, cross: 125}
+const saint = {x: -40, y: 200, Xjump: 80, Yjump: 10}
+const devil = {x: 10, y: 200, Xjump: 100, Yjump: 10}
 const Devils = () =>
 {
-    var sOnBoat = false;
-    var dOnBoat = false;
-    var boatLeft = true;
-    const [dx, setDx] = useState(10)
-    const [dy, setDy] = useState(200)
-    const [sx, setSx] = useState(-40)
-    const [sy, setSy] = useState(200)
-    const [bx, setBx] = useState(-190)
-    const [by, setBy] = useState(260)
+    const [dx, setDx] = useState(devil.x)
+    const [dy, setDy] = useState(devil.y)
+    const [sx, setSx] = useState(saint.x)
+    const [sy, setSy] = useState(saint.y)
+    const [bx, setBx] = useState(boat.x)
+    const [by, setBy] = useState(boat.y)
     const restartGame = () =>
     {
         setDx(10)
@@ -20,6 +25,9 @@ const Devils = () =>
         setSy(200)
         setBx(-190)
         setBy(260)
+        sOnBoat = false;
+        dOnBoat = false;
+        boatLeft = true;
     }
     const handleClick = (type) =>
     {
@@ -39,17 +47,23 @@ const Devils = () =>
             }
             else if (sOnBoat && !boatLeft)
             {
-                setSx(0)
+                setSx(200)
                 setSy(200)
                 sOnBoat = false
+            }
+            else if (!sOnBoat && !boatLeft)
+            {
+                setSx(saint.x + saint.Xjump + boat.cross)
+                setSy(saint.y + saint.Yjump)
+                sOnBoat = true
             }
         }
         else if (type === 'devil')
         {
             if (!dOnBoat && boatLeft)
             {
-                setDx(110)
-                setDy(210)
+                setDx(devil.x + devil.Xjump)
+                setDy(devil.y + devil.Yjump)
                 dOnBoat = true
             }
             else if (dOnBoat && boatLeft)
@@ -58,28 +72,70 @@ const Devils = () =>
                 setDy(200)
                 dOnBoat = false
             }
+            else if (dOnBoat && !boatLeft)
+            {
+                setDx(devil.x + 2*devil.Xjump + boat.cross)
+                setDy(devil.y)
+                dOnBoat = false
+            }
+            else if (!dOnBoat && !boatLeft)
+            {
+                setDx(devil.x + devil.Xjump + boat.cross)
+                setDy(devil.y + devil.Yjump)
+                dOnBoat = true
+            }
+            // Debug devil
+            //console.log("Devil is on boat:", dOnBoat)
         } 
         else if (type === 'boat')
         {
-            if (boatLeft && sOnBoat && dOnBoat)
+            if (sOnBoat && dOnBoat)
             {
-                setBx(-65)
-                setSx(-40 + 80 + 125)
-                setDx(110 + 125)
-                boatLeft = false
+                if (boatLeft)
+                {
+                    setBx(boat.x + 125)
+                    setSx(-40 + 80 + 125)
+                    setDx(110 + 125)
+                    boatLeft = false
+                }
+                else if (!boatLeft)
+                {
+                    setBx(boat.x)
+                    setDx(110)
+                    setSx(40)
+                    boatLeft = true
+                }
             }
-            else if (boatLeft && sOnBoat)
+            else if (sOnBoat && !dOnBoat)
             {
-                setBx(-65)
-                setSx(-40 + 80 + 125)
-                boatLeft = false
+                if (boatLeft)
+                {
+                    setSx(saint.x + saint.Xjump + boat.cross)
+                    setBx(boat.x + boat.cross)
+                    boatLeft = false
+                }
+                else if (!boatLeft)
+                {
+                    setSx(saint.x + saint.Xjump)
+                    setBx(boat.x)
+                    boatLeft = true
+                }
             }
-            else if (!boatLeft)
+            else if (!sOnBoat && dOnBoat)
             {
-                setBx(-190)
-                boatLeft = true
+                if (boatLeft)
+                {
+                    setBx(boat.x + boat.cross)
+                    setDx(devil.x + devil.Xjump + boat.cross)
+                    boatLeft = false
+                }
+                else if (!boatLeft)
+                {
+                    setBx(boat.x)
+                    setDx(devil.x + devil.Xjump)
+                    boatLeft = true
+                }
             }
-            
         }
     }
     return (
@@ -90,10 +146,10 @@ const Devils = () =>
             <div className='river'></div>
             <button className='cha devil' 
                 onClick={() => handleClick('devil')} 
-                style={{top: dy + "px", left: dx + "px"}}></button>
+                style={{top: dy + "px", left: dx + "px"}}>1</button>
             <button className='cha saint' 
                 onClick={() => handleClick('saint')}
-                style={{top: sy + "px", left: sx + "px"}}></button>
+                style={{top: sy + "px", left: sx + "px"}}>1</button>
             <button className='boat'
                 onClick={() => handleClick('boat')}
                 style={{top: by + "px", left: bx + "px"}}></button>
